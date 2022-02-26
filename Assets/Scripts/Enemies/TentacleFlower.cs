93ea969db5e955d3ac5e687e837af6618845939e
Zach <53870574+Jadefire16@users.noreply.github.com>
2022-02-26 12:07:14 -0500

    Added TentacleFlower
    Added the TentacleFlower script and created some gizmos to see it's attacks
    Red is the radius for the whip attack
    Blue is approximately the length of the spit attack

---
+++ Assets/Scripts/Enemies/TentacleFlower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleFlower : MonoBehaviour // you can definitely move this to a state machine later and recycle this for the controller
+{
+
+    [SerializeField] private float spitAttackRange = 6;
+    [SerializeField] private float whipAttackRadius = 3;
+
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void SpitAttack()
+    {
+
+    }
+
+    private void WhipAttack()
+    {
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, whipAttackRadius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position + new Vector3(0,0,(int)(spitAttackRange * 0.5f)), new Vector3(0.25f, 0.25f, spitAttackRange));
+    }
+
+}
